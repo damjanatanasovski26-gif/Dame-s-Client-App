a0b75fadb4f1f09e71f386c6d133ae6771930f3a
damjanatanasovski26-gif <damjan.atanasovski26@gmail.com>
2026-05-21 20:54:56 +0200

    Tighten nutrition footer controls

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "trainer-app-v38";
+const CACHE_NAME = "trainer-app-v39";
 const APP_SHELL = [
   "/login",
   "/manifest.webmanifest?v=4",
-  "/static/style.css?v=38",
+  "/static/style.css?v=39",
   "/static/images/coach.png?v=2",
   "/static/images/social/instagram.png",
   "/static/images/social/facebook.png",
