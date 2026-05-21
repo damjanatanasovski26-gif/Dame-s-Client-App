3bdb108c82f7c505d5e0b1e5f3f07d05b7f4823b
damjanatanasovski26-gif <damjan.atanasovski26@gmail.com>
2026-05-21 21:14:31 +0200

    Polish nutrition form labels

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "trainer-app-v40";
+const CACHE_NAME = "trainer-app-v41";
 const APP_SHELL = [
   "/login",
   "/manifest.webmanifest?v=4",
-  "/static/style.css?v=40",
+  "/static/style.css?v=41",
   "/static/images/coach.png?v=2",
   "/static/images/social/instagram.png",
   "/static/images/social/facebook.png",
